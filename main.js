a9ff68534a0f5577cd6ae8784d55b37f857d3532
ajaygajra <ajay.gajra@inscripts.in>
2018-01-31 16:16:12 +0530

    mysql error while fetching

--- main.js
+++ main.js
@@ -2,33 +2,16 @@ var express = require("express");
 var routes = require('./routes/routes.js');
 var userModle = require('./models/users.js');
 var app = express();
-const Sequelize=require('sequelize');
-const sequelize=require('./mysql/database');
-const UserMeta=sequelize.define("userMeta",{
-  firstName: {
-      type: Sequelize.STRING
-    },
-    lastName: {
-      type: Sequelize.STRING
-    }
-  });
-  UserMeta.sync().then(function(arg1,arg2){
 
-    arg1.create({
-      firstName: 'John',
-      lastName: 'Hancock'
-    });
-  });
-UserMeta.findAll().then(users=>{
-
-  users.forEach(function(currentValue, index, arr){
-    user=JSON.parse(JSON.stringify(currentValue),true);
-    console.log(user.createdAt);
-  });
-});
+const UserMeta = require('./models/user.js');
 
 routes.use('/api/v1/', require('./routes/openRoutes.js'));
 app.use(routes);
 
-app.listen(9090, function() {
-});
+app.listen(8081, function() {
+    UserMeta
+        .sync()
+        .then(function(arg1) {
+            console.log(arg1 + "table created");
+        });
+    });
---
+++ models/user.js
@@ -0,0 +1,62 @@
+const Sequelize = require('sequelize');
+const sequelize = require('./../mysql/database.js');
+
+const UserMeta = sequelize.define("Meta", {
+    id:{
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        validate: {
+            allowNull: false,
+            notEmpty: true
+        }
+    },
+    firstName: {
+        type: Sequelize.STRING,
+        defaultValue: 'user',
+        allowNull: false,
+        notEmpty: true
+
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        defaultValue: 'name',
+        allowNull: false,
+        notEmpty: true
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique: 'compositeIndex',
+        allowNull: false,
+        notEmpty: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    user_id: {
+        type: Sequelize.STRING,
+        unique: 'compositeIndex',
+        allowNull: false
+    },
+    reg_time: {
+        type: Sequelize.BIGINT(11),
+        defaultValue: 0,
+        allowNull: false
+    },
+    meta_info: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+        defaultValue: null
+    },
+    contact: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+        defaultValue: null
+    }
+
+});
+module.exports = UserMeta;
