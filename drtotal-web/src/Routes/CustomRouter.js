cf567b4b94cdf70ee11788773348d094ca1d718c
ins-201 <ajay.gajra@inscripts.in>
2018-07-24 18:45:43 +0530

    LoginDone

--- drtotal-web/src/Routes/CustomRouter.js
+++ drtotal-web/src/Routes/CustomRouter.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Route, Link,Switch} from "react-router-dom";
 import {LoginPage} from '../Login/Components/Containers/LoginContainer';
 import Redirect from 'react-router-dom/Redirect';
 import {NavBar} from '../NAV/NavBar'
 import {connect} from 'react-redux';
-import { Navbar } from 'react-bootstrap';
-import LoginForm from '../Login/Components/Containers/LoginForm';
 
-
-
-
+/**
+ * 
+ * @param {*} props
+ * Detects and redirects to apropriate routes. 
+ */
 const DrTotalRouter = (props) => <Router>
   <div>
+  <Switch>
+
     <Route
       exact
       path="/"
       component={() => {
+
       if (props.login) {
         switch (props.stage) {
-          case 'login':
+
+          case 'login':          
           if(props.showRoles)
-            return <Redirect to='/signup'/>  
+          return <Redirect to='/signup'/>  
 
             return (              
               <div><NavBar/>
@@ -32,39 +36,55 @@ const DrTotalRouter = (props) => <Router>
             return (              
               <Redirect to='/signup'/>
             );
-          default:
-            return (
-              <h1>Cool</h1>
+          
+            default:
+            
+            return (              
+              <div><NavBar/>
+                <p>{JSON.stringify(props.user)} </p>       
+              </div>
             );
         }
       } else {
+
         if(props.showRoles)
             return <Redirect to='/signup'/>            
-        return (<Redirect to='/login'/>)
+          return (<Redirect to='/login'/>)
       }
     }}/>
 
     <Route
+      exact
       path="/login"
-      component={() => (
-      <LoginPage errorCode=' ' type='login'></LoginPage>
+      component={() => (        
+      <LoginPage errorCode=' ' type={props.stage}></LoginPage>
     )}/>
 
     <Route
+    exact
       path="/signup"
       component={() => {
       return (
         <LoginPage errorCode=' ' type='signup'></LoginPage>
       )
     }}/>
+    <Route
+    
+    component={()=>(<h1>hello world!!!!</h1>)}
+    />
+    </Switch>
   </div>
+  
+
 </Router>
+
+
 const mapStateToProps = (state, ownProps) => {
-  if (state.Login.login) {
-    console.log(state.Login)
+  if (state.Login.login) {    
     return state.Login;
   } else {
     return {}
   }
 };
+
 export const CustomRouter = connect(mapStateToProps, {})(DrTotalRouter);
